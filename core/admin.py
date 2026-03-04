3d949f14c0d862cbdb14f6be6a6235b215a5a3d2
TilovovSherzod <tilovovsherzod@github.com>
2026-03-04 15:18:06 +0500

    Ignore local/IDE files

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+import nested_admin
 from .models import Presentation, Guide, Practical, Test, Question, Choice, TestAttempt, Answer
 
 
@@ -25,35 +26,61 @@ class PracticalAdmin(admin.ModelAdmin):
 
 
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(nested_admin.NestedTabularInline):
+    """Variantlarni savolni ichida ko'rsatish uchun"""
     model = Choice
     extra = 1
+    fields = ('text', 'is_correct')
+    verbose_name_plural = "Javob variantlari"
+    readonly_fields = ()
+    can_delete = True  # O'chirish imkoniyatini berish
+    
+    class Media:
+        css = {
+            'all': ('admin_choice_inline.css',)
+        }
+        js = ('admin_choice_inline.js',)
 
 
-class QuestionInline(admin.StackedInline):
+class QuestionInline(nested_admin.NestedTabularInline):
+    """Savollarni test ichida ko'rsatish uchun"""
     model = Question
     extra = 1
+    fields = ('text', 'order', 'points', 'allow_multiple')
+    ordering = ('order',)
+    verbose_name_plural = "Savollar"
+    inlines = [ChoiceInline]  # Savolning ichida variantlar
+    can_delete = True  # O'chirish imkoniyatini berish
 
 
 @admin.register(Test)
-class TestAdmin(admin.ModelAdmin):
+class TestAdmin(nested_admin.NestedModelAdmin):
     list_display = ('title', 'file', 'is_published', 'created_at')
     search_fields = ('title',)
-    # include question inline so questions can be edited when editing a Test
-    inlines = [QuestionInline]
-    # keep Test visible as the single test entry in admin
+    list_filter = ('is_published',)
+    fieldsets = (
+        (None, {'fields': ('title', 'description', 'file')}),
+        ('Sozlamalar', {'fields': ('is_published', 'time_limit', 'allow_manual_review')}),
+    )
+    inlines = [QuestionInline]  # Testning ichida savollar, savolning ichida variantlar
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('text', 'test', 'order', 'points', 'allow_multiple')
+    list_filter = ('test', 'allow_multiple')
     search_fields = ('text', 'test__title')
+    fieldsets = (
+        ('Savol ma\'lumotlari', {'fields': ('test', 'text', 'order', 'points')}),
+        ('Variantlar', {'fields': ('allow_multiple',), 'description': 'Bir nechta to\'g\'ri javob bo\'lsa, "Bir nechta variantga ruxsat berish" ni belgilang'}),
+    )
     inlines = [ChoiceInline]
 
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('text', 'question', 'is_correct')
+    list_filter = ('is_correct', 'question__test')
     search_fields = ('text', 'question__text')
 
 
